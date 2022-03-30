5d64afe753f5f58e2b7087ad11a05771b5f66331
baris.gorgun <barisgorgunn@gmail.com>
2022-03-30 12:12:01 +0300

    Added package file

---
+++ Package.swift
@@ -0,0 +1,48 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CPaaSSDK",
+    platforms: [
+                .iOS(.v10)
+               ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "CPaaSSDK",
+            targets: ["WebRTC","CPaaSSDKFramework","iOSLibrary"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/barisgorgun/SPMWebRTC.git", branch: "master"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "WebRTC",
+            dependencies: [.product(name: "WebRTC", package: "SPMWebRTC")]),
+        .target(
+                    name: "iOSLibrary",
+                    cSettings: [
+                        .define("OTHER_LDFLAGS", to: "-ObjC")
+                    ], linkerSettings: [
+                        .linkedFramework("AudioToolbox"),
+                        .linkedFramework("VideoToolbox"),
+                        .linkedFramework("SystemConfiguration"),
+                        .linkedFramework("AVFoundation"),
+                        .linkedFramework("GLKit"),
+                        .linkedFramework("PushKit"),
+                        .linkedLibrary("c++"),
+                        .linkedLibrary("z"),
+                        .linkedLibrary("icucore"),
+                    ]),
+        
+        .binaryTarget(name: "CPaaSSDKFramework",
+                      path: "CPaaSSDK.xcframework"),
+       
+    ]
+)
