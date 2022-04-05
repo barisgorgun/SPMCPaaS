999136f425999d0b207370a93cd9fbddbb4ab0c8
baris.gorgun <barisgorgunn@gmail.com>
2022-04-05 11:39:30 +0300

    sync

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CPaaSSDK",
     platforms: [
-                .iOS(.v12)
+                .iOS(.v10)
                ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/barisgorgun/SPMWebRTC.git", branch: "master"),
+        .package(url: "https://github.com/barisgorgun/SPMWebRTC.git", branch: "packageFile"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,29 +27,28 @@ let package = Package(
             dependencies: [.product(name: "WebRTC", package: "SPMWebRTC")]),
         .target(
                     name: "iOSLibrary",
-                    cSettings: [
-                        .define("OTHER_LDFLAGS", to: "-ObjC")
-                    ], linkerSettings: [
+                    linkerSettings: [
                         .linkedFramework("AudioToolbox"),
+                        .linkedFramework("PushKit"),
                         .linkedFramework("VideoToolbox"),
+                        .linkedFramework("GLKit"),
+                        .linkedFramework("CoreGraphics"),
+                        .linkedFramework("CoreVideo"),
+                        .linkedFramework("CoreMedia"),
+                        .linkedFramework("CoreAudio"),
+                        .linkedFramework("QuartzCore"),
+                        .linkedFramework("OpenGLES"),
+                        .linkedFramework("Security"),
+                        .linkedFramework("CFNetwork"),
                         .linkedFramework("SystemConfiguration"),
                         .linkedFramework("AVFoundation"),
-                        .linkedFramework("GLKit"),
-                        .linkedFramework("PushKit"),
-                        .linkedFramework("CFNetwork"),
-                        .linkedFramework("Security"),
-                        .linkedFramework("OpenGLES"),
-                        .linkedFramework("QuartzCore"),
-                        .linkedFramework("CoreAudio"),
-                        .linkedFramework("CoreMedia"),
-                        .linkedFramework("CoreVideo"),
-                        .linkedFramework("CoreGraphics"),
                         .linkedLibrary("c++"),
                         .linkedLibrary("z"),
                         .linkedLibrary("icucore"),
                         .linkedLibrary("sqlite3"),
                         .linkedLibrary("stdc++"),
                     ]),
+        
         .binaryTarget(name: "CPaaSSDKFramework",
                       path: "CPaaSSDK.xcframework"),
        
